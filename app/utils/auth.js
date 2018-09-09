ffb9e0ffa32f80e75d49651599987b9fe0b00964
Michael Simmons <github@e7i.org>
2018-09-09 11:56:19 +0800

    feat(auth): Finished jwt auth lib

--- app/utils/auth.js
+++ app/utils/auth.js
@@ -1,24 +1,128 @@
+/**
+ * Lots of testing in here for window and localStorage since I may
+ * create an Electron app with this at some point.
+ *
+ * Will need to refactor and add Electron methods for getting/storing
+ * the token at some point, will tackle then.
+ */
+
 import jwtDecode from 'jwt-decode';
+import queryString from 'query-string';
+import { AUTH_TOKEN_NAME } from 'utils/constants';
 
-export const TOKEN_NAME = 'token';
+/**
+ * Check whether the user is authenticated.
+ *
+ * @return {boolean} True or false.
+ */
+export const isAuthenticated = () => getUser() !== false;
 
-export const isAuthenticated = () => {
-  if (localStorage && localStorage.getItem(TOKEN_NAME)) {
-    try {
-      jwtDecode(localStorage.getItem(TOKEN_NAME));
-      return true;
-    } catch (err) {
-      console.error('Could not decoded token.');
-    }
+/**
+ * Retrieve the user object from the stored token.
+ *
+ * @return {user|false} User object or false.
+ */
+export const getUser = () => {
+  try {
+    return jwtDecode(getToken());
+  } catch (err) {
+    return false;
+  }
+};
+
+/**
+ * Return an object with an Authorization Bearer header
+ * for use with any http request library.
+ *
+ * @return {object} Object with auth header or empty.
+ */
+export const authHeader = () => {
+  const token = getToken();
+
+  if (token) {
+    return { Authorization: `Bearer ${token}` };
+  }
+
+  return {};
+};
+
+/**
+ * Retrieve the token appropriately, save it if necessary.
+ *
+ * @return {any} Token or false.
+ */
+export const getToken = () => {
+  if (getTokenFromLocalStorage()) {
+    return getTokenFromLocalStorage();
+  }
+
+  const { token } = getTokenFromHash();
+  if (token) {
+    storeTokenLocalStorage(token);
+
+    return token;
   }
 
   return false;
 };
 
-export const authHeader = () => {
-  if (localStorage.getItem('token')) {
-    return { Authorization: `Bearer ${localStorage.getItem('token')}` };
+/**
+ * Internal. Retrieve the token from browser localStorage.
+ *
+ * @return {any} Token string, or false|null.
+ */
+const getTokenFromLocalStorage = () => {
+  if (typeof localStorage !== 'object' || typeof localStorage.getItem !== 'function') {
+    return false;
   }
 
-  return {};
+  return localStorage.getItem(AUTH_TOKEN_NAME);
+};
+
+/**
+ * Internal. Retrieve the token string from the window location hash.
+ *
+ * @return {any} Token string, or false|null.
+ */
+const getTokenFromHash = () => {
+  if (typeof window === 'undefined' || !window.location.hash) {
+    return false;
+  }
+
+  const hash = queryString(window.location.hash);
+
+  return hash && hash.token ? hash.token : false;
+};
+
+/**
+ * Internal. Store the token in the browser's localStorage.
+ * Typically called after OAuth redirects to us.
+ *
+ * @return {boolean} True on success, false on error.
+ */
+const storeTokenLocalStorage = (token) => {
+  if (typeof localStorage !== 'object' || typeof localStorage.setItem !== 'function') {
+    return false;
+  }
+
+  localStorage.setItem(AUTH_TOKEN_NAME, token);
+  clearLocationHash();
+
+  return true;
+};
+
+/**
+ * Internal. Clear the window location hash, likely after retrieving
+ * and storing the token.
+ *
+ * @return {boolean} True on success, false on error.
+ */
+const clearLocationHash = () => {
+  if (typeof window !== 'object' || typeof window.location !== 'object') {
+    return false;
+  }
+
+  window.location.hash = '';
+
+  return true;
 };
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,4 @@
+export const AUTH_TOKEN_NAME = 'jwt';
 export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
 export const DAEMON = '@@saga-injector/daemon';
 export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
