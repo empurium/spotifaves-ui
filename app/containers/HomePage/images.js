ca82383e6cf5e9b618d307878047b6ce862dac16
Michael Simmons <github@e7i.org>
2018-09-14 18:30:47 +0800

    style(images): Use real Artist images

--- app/containers/HomePage/images.js
+++ app/containers/HomePage/images.js
@@ -1,40 +1,168 @@
 const images = [
-  { src: '//unsplash.it/250/250', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/251', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/252', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/253', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/254', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/255', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/251/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/252/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/253/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/254/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/255/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/256/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/250', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/251', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/251/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/252/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/253/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/254/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/255/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/256/256', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/250', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/252', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/253', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/254', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/250', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/251', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/252', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/253', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/254', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/250', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/251', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/252', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/253', alt: 'Artist artwork.' },
-  { src: '//unsplash.it/250/254', alt: 'Artist artwork.' },
+  {
+    src: 'https://i.scdn.co/image/7891384e95fa3c1a7020645ae790c7924f7ae1dc',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/1502a4c71bb7f70dc1b23333b7cf5b5db5357158',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/abe4d6117d4cbedad952bbf4d9b88d4922cb0952',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/cb4fa9525cadb7f419f43e973133ff4803123324',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/e884e3f6b4b120f59ea16050deb44ec473fa38b1',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/d8ac2ce27d586ed217ccc9dfc5c9c4125c2c8076',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/9527e700b5c80aa67350d5de0da10a0aa754fcb1',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/35a323eca5accb9b1281030efe345013a961d7c2',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/1c86e3586e0d7bf491045ec0b5ac57d4469d8202',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/9f8b45433eb66d70e51179437df2ff0f19aa21f1',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/ebaabbd21b854b86c5a213b91b1aeef90b2ffb5d',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/e45c3f34b1a88245f5c8ef16c5b9fb6b0fe1c8a9',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/1e022a7eddaf32d38cf84e000f49e64dc3fd7f80',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/74d44570990858341836261beb39b3faa18f7603',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/136adacdcc3c258b3798eb24a9f4dd9e80894682',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/f07f00600432c21013f8e30c623cffa8df08d418',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/5f7c97e95398da3c8435c8d57db4cf3b278c6128',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/36189c388c0953f0e63c15840870e0ac15cf39c5',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/183928e879d0dd0df2e68e790a2d9ec4875220a3',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/61e4a642d4a0a9302883b6321e7e5a11b2220838',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/898eaac95e7e86b9737b056ce711b67d239ef7c8',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/7ac3699438281d5767a4f3b1ed321c2ecc05127a',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/9c027be99ff64285da9914d84852bef883f01b7d',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/c358aeee88e883a72c9370c4279b2223079fe523',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/3e0c298c3f685ab6b225ccd22281b030a31ba5c3',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/f8e461240f5a4bec69b033fdb621a3f3384fdc02',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/fbd2c1197ca085817485ab216c1f4621ca3a8f57',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/3fdd6e8b9ec060705eb32a2f157cbf7771259cd4',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/fdce941a043157ea170ea8b6c0debc3cbadb6264',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/2d029b5c18df1a1e49e94221a30bb5f07de6726b',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/c8a1da0965c9476f37134884d4a50af825fd494d',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/65f888e6a8e57da21b96346e473bb6d0e0f62161',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/b39fc78bc903a866049e7b1c3ccf1f5935b1985c',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/c2b848d16e161b9b1c85958df72fd8e8e2fba7b3',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/f5af8b6d988d1e2742a0cf045912c9cd676055bc',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/27f2866812dbeb8235d499175efdba3fca631384',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/4d96a50739be24fe34ee7c64e9201b8d30366c18',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/ac811f08839e11ebf89e3bea5ed08f73f3e81c4d',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/1da54c0262d01c3524363f6e963edb2e1a6c2416',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/1ef3b3cbd562777fed4dec39caf3c6d4a508550d',
+    alt: 'Artist artwork.',
+  },
+  {
+    src: 'https://i.scdn.co/image/df9086189e64ad3f3dec425f53b0c5f595a787b5',
+    alt: 'Artist artwork.',
+  },
 ];
 
 export default images;
